c37f7aab7b905f907fac3f83776faba19e14596b
Gabriel Carvalhal <gabrielccarvalhal@hotmail.com>
2026-06-20 16:57:03 +0000

    int_orders table

--- models/marts/finance/refactoring/fct_customer_orders.sql
+++ models/marts/finance/refactoring/fct_customer_orders.sql
@@ -1,30 +1,10 @@
 -- with statement
 with
-    -- logical ctes
-    orders_payment_success as (
-        select
-            payments.order_id,
-            max(payments.created) as payment_finalized_date,
-            sum(payments.amount) / 100.0 as total_amount_paid
-        from {{ ref("_stg_payments") }} as payments
-        group by 1
-    ),
 
     paid_orders as (
-        select
-            orders.order_id,
-            orders.customer_id,
-            orders.order_placed_at,
-            orders.order_status,
-            p.total_amount_paid,
-            p.payment_finalized_date,
-            customer.customer_first_name,
-            customer.customer_last_name
-        from {{ ref("_stg_orders") }} as orders
-        left join orders_payment_success as p on orders.order_id = p.order_id
-        left join {{ ref("_stg_customers") }} customer on orders.customer_id = customer.customer_id
+        select * from {{ ref('int_orders') }}
     ),
-
+    
     order_amount_paid as (
         select p.order_id, sum(t2.total_amount_paid) as clv_bad
         from paid_orders p
