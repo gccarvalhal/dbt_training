fd2379308a458619bacdedfa51d9be5efb237f6b
Gabriel Carvalhal <gabrielccarvalhal@hotmail.com>
2026-08-02 13:49:20 +0000

    add env

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,8 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%} {{ default_schema }}
-
+    {% set env= env_var('DBT_ENV_NAME') -%}
+    {%- if custom_schema_name is none or env !='prod' -%} 
+        {{ default_schema }}
     {%- else -%} {{ custom_schema_name | trim }}
 
     {%- endif -%}
